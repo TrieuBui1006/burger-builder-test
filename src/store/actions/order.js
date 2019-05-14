88f274bbd36ec3eb7faf80ca97fb12ab3c094128
BUI Duc Trieu <buiductrieu1006@gmail.com>
2019-05-14 11:25:14 +0700

    add utility

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -35,3 +35,51 @@ export const purchaseBurger = (orderData) => {
             });
     };
 }; 
+
+export const purchaseInit = () => {
+    return {
+        type: actionTypes.PURCHASE_INIT
+    };
+}
+
+export const fetchOrdesuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrderFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+export const fetchOrderStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrderStart());
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchOrders = [];
+                for (let key in res.data) {
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdesuccess(fetchOrders));
+            })
+            .catch(err => {
+                dispatch(fetchOrderFail(err));
+            });
+    }
+};
+
+
